2aae913704e03922c778cb686d4209f7d93d93f9
Tim Knoll <timothy.knoll@gmail.com>
2018-09-03 19:45:48 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot (phrase = Squawk)
+const = "Squawk!"
+def parrot (phrase)
   puts "#{phrase}"
   return "#{phrase}"
 end
